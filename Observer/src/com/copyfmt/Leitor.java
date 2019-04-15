2d4796e351427dc6acb91f3e2539d845bade62a8
copyfmt <v.escame@gmail.com>
2019-04-14 21:51:46 -0300

    exercicio observer

---
+++ Observer/src/com/copyfmt/Leitor.java
@@ -0,0 +1,20 @@
+package com.copyfmt;
+
+public class Leitor implements Observer{
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void update(Revista r) {
+		System.out.println(this.name + " está lendo o artigo " + r.artigo());
+	}
+
+}
